9d48d6d3b7624940ab397e99ad18bafe98ee602a
Mervin-James <43912182+Mervin-James@users.noreply.github.com>
2020-12-02 02:14:53 -0500

    removed unnecessary console.log's

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -84,7 +84,6 @@ module.exports.updateCampground = async (req, res) => {
             await cloudinary.uploader.destroy(filename);  //removes image from cloudinary
         }
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
-        console.log(campground);
     }
     req.flash('success', "Successfully updated campground!");
     res.redirect(`/campgrounds/${campground._id}`)
